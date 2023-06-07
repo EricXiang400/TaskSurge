934bf0135a69699e76c3d3079fe5d529edd2a877
Eric Xiang <b13967168053@berkeley.edu>
2023-06-06 23:45:48 -0400

    Add date comparison and dynamic list for date, but still pull data from bundle

--- ToDoListApp/Model/TodoContent.swift
+++ ToDoListApp/Model/TodoContent.swift
@@ -11,4 +11,25 @@ struct TodoContent: Hashable, Codable, Identifiable {
     var id: Int
     var content: String
     var completed: Bool
+    var date: Date
+    enum CodingKeys: String, CodingKey {
+        case id
+        case content
+        case completed
+        case date
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        content = try container.decode(String.self, forKey: .content)
+        completed = try container.decode(Bool.self, forKey: .completed)
+        let dateString = try container.decode(String.self, forKey: .date)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        if let date = dateFormatter.date(from: dateString) {
+            self.date = date
+        } else {
+            throw fatalError("Invalid Date Format")
+        }
+    }
 }
--- ToDoListApp/Views/TodoListView.swift
+++ ToDoListApp/Views/TodoListView.swift
@@ -9,7 +9,9 @@ import Foundation
 import SwiftUI
 
 struct TodoListView: View {
+//    private var t = removeDataFromDocumentDirectory(fileName: "data.json")
     @State private var todos: [TodoContent] = loadData(fileName: "data.json")
+    @EnvironmentObject private var selectedDate: SelectedDate
     
     func saveData() {
         do {
@@ -27,13 +29,30 @@ struct TodoListView: View {
         let data: Data
         do {
             let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            let fileURL = documentDirectory.appendingPathComponent("data.json")
+//            let fileURL = documentDirectory.appendingPathComponent("data.json")
+            let fileURL = Bundle.main.url(forResource: fileName, withExtension: nil)!
             data = try Data(contentsOf: fileURL)
             let decoder = JSONDecoder()
             return try decoder.decode([TodoContent].self, from: data)
         } catch {
             fatalError("Error encoding or writing")
         }
+    }
+    static func removeDataFromDocumentDirectory(fileName: String) {
+        do {
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            
+            let fileURL = documentDirectory.appendingPathComponent(fileName)
+            try FileManager.default.removeItem(at: fileURL)
+            
+            print("File removed successfully: \(fileName)")
+        } catch {
+            print("Error removing file: \(error)")
+        }
+    }
+    func sameDate(date1: Date, date2: Date) -> Bool {
+        let res = Calendar.current.compare(date1, to: date2, toGranularity: .day)
+        return res == .orderedSame
     }
     
     var body: some View {
@@ -42,12 +61,17 @@ struct TodoListView: View {
                 var todoIndex: Int {
                     $todos.firstIndex(where: {$0.id == todo.id})!
                 }
-                HStack(spacing: 20) {
-                    SelectionButton(completed: $todos[todoIndex].completed)
-                        .padding(5)
-                    TextField("contefasnt", text: $todos[todoIndex].content, onCommit: saveData)
-                    Spacer()
+                if sameDate(date1: selectedDate.selectedDate, date2: todo.date) {
+                    HStack {}
+                } else {
+                    HStack(spacing: 20) {
+                        SelectionButton(completed: $todos[todoIndex].completed)
+                            .padding(5)
+                        TextField("contefasnt", text: $todos[todoIndex].content, onCommit: saveData)
+                        Spacer()
+                    }
                 }
+                
             }
         }
     }
