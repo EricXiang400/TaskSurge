8858d2ffef862df870d15d1fa571d8ae2dbc9816
Eric Xiang <b13967168053@berkeley.edu>
2023-07-02 16:50:31 +0800

    Refine progress bar with red/green color and use geometry reader to calculate geometry

--- ToDoListApp/Views/ProgressBarView.swift
+++ ToDoListApp/Views/ProgressBarView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 
 
 struct ProgressBarView: View {
-    
     @State private var totalProgress: Float = 100
     @Binding var todoContent: TodoContent
     var body: some View {
-        ProgressView(value: todoContent.progress, total: totalProgress)
+        ProgressView(value: 10.0, total: totalProgress)
+            .progressViewStyle(CustomProgressViewStyle())
+    }
+}
+
+struct CustomProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let greenColor = Color(red: 0, green: 0.7, blue: 0)
+        let redColor = Color(red: 0.7, green: 0, blue: 0)
+    
+        return GeometryReader { geometry in
+            VStack {
+                Spacer()
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                        .foregroundColor(redColor)
+
+                    Rectangle()
+                        .frame(width: CGFloat(configuration.fractionCompleted ?? 0) * geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                        .foregroundColor(greenColor)
+                }
+                Spacer()
+            }
+            
+        }
     }
 }
